aa98d71cc300e02c0154d83a995ae8f1b8542a06
Younes Karim <cph-yk31@cphbusiness.dk>
2021-05-06 09:41:11 +0200

    update

--- Diagramer/carport creation.sql
+++ Diagramer/carport creation.sql
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS `carport`.`order` (
   `order_id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `orderdate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `shed_id` INT NULL,
   PRIMARY KEY (`order_id`, `user_id`),
   INDEX `fk_order_user_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_user`
@@ -55,7 +56,6 @@ CREATE TABLE IF NOT EXISTS `carport`.`material` (
   `length` DOUBLE NULL,
   `width` DOUBLE NULL,
   `price` DOUBLE NOT NULL,
-  `category_id` INT NOT NULL,
   PRIMARY KEY (`material_id`))
 ENGINE = InnoDB;
 
@@ -65,7 +65,6 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `carport`.`carport` (
   `carport_id` INT NOT NULL AUTO_INCREMENT,
-  `shed_id` INT NULL,
   `order_id` INT NOT NULL,
   `total` DOUBLE NOT NULL,
   PRIMARY KEY (`carport_id`),
@@ -79,22 +78,40 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `carport`.`shed`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `carport`.`shed` (
+  `shed_id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `total` DOUBLE NOT NULL,
+  PRIMARY KEY (`shed_id`),
+  INDEX `fk_shed_order1_idx` (`order_id` ASC) VISIBLE,
+  CONSTRAINT `fk_shed_order1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `carport`.`order` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `carport`.`shed_has_material_list`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `carport`.`shed_has_material_list` (
-  `shed_id` INT NOT NULL AUTO_INCREMENT,
+  `shed_id` INT NOT NULL,
   `material_id` INT NOT NULL,
   `quantity` FLOAT NOT NULL,
-  PRIMARY KEY (`shed_id`, `material_id`),
+  PRIMARY KEY (`material_id`, `shed_id`),
   INDEX `fk_shed_has_material_list_material1_idx` (`material_id` ASC) VISIBLE,
+  INDEX `fk_shed_has_material_list_shed1_idx` (`shed_id` ASC) VISIBLE,
   CONSTRAINT `fk_shed_has_material_list_material1`
     FOREIGN KEY (`material_id`)
     REFERENCES `carport`.`material` (`material_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_shed_has_material_list_carport1`
+  CONSTRAINT `fk_shed_has_material_list_shed1`
     FOREIGN KEY (`shed_id`)
-    REFERENCES `carport`.`carport` (`shed_id`)
+    REFERENCES `carport`.`shed` (`shed_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -129,10 +146,26 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `carport`.`material_category` (
   `material_category_id` INT NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(45) NULL,
-  PRIMARY KEY (`material_category_id`),
-  CONSTRAINT `fk_material_category_material1`
+  PRIMARY KEY (`material_category_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `carport`.`material_has_material_category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `carport`.`material_has_material_category` (
+  `material_id` INT NOT NULL,
+  `material_category_id` INT NOT NULL,
+  PRIMARY KEY (`material_id`, `material_category_id`),
+  INDEX `fk_material_has_material_category_material_category1_idx` (`material_category_id` ASC) VISIBLE,
+  CONSTRAINT `fk_material_has_material_category_material1`
+    FOREIGN KEY (`material_id`)
+    REFERENCES `carport`.`material` (`material_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_material_has_material_category_material_category1`
     FOREIGN KEY (`material_category_id`)
-    REFERENCES `carport`.`material` (`category_id`)
+    REFERENCES `carport`.`material_category` (`material_category_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
