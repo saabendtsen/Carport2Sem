0ccb8b1ba091aef6f336278c54c7eefa9a3d41fe
Younes Karim <cph-yk31@cphbusiness.dk>
2021-05-10 10:49:17 +0200

    fix db creation+dump

--- Diagramer/carport creation.sql
+++ Diagramer/carport creation.sql
@@ -1,186 +1,189 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `carport` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `carport`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: carport
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema carport
--- -----------------------------------------------------
+--
+-- Table structure for table `carport`
+--
 
--- -----------------------------------------------------
--- Schema carport
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `carport` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `carport` ;
-
--- -----------------------------------------------------
--- Table `carport`.`user`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`user` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(16) NOT NULL,
-  `role` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `carport`.`order`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`order` (
-  `order_id` INT NOT NULL AUTO_INCREMENT,
-  `user_id` INT NOT NULL,
-  `orderdate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`order_id`, `user_id`),
-  INDEX `fk_order_user_idx` (`user_id` ASC) VISIBLE,
-  CONSTRAINT `fk_order_user`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `carport`.`user` (`user_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `carport`.`carport`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`carport` (
-  `carport_id` INT NOT NULL AUTO_INCREMENT,
-  `order_id` INT NOT NULL,
-  `total` DOUBLE NULL DEFAULT NULL,
-  `length` DOUBLE NOT NULL,
-  `width` DOUBLE NOT NULL,
+DROP TABLE IF EXISTS `carport`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carport` (
+  `carport_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `total` double DEFAULT NULL,
+  `length` double NOT NULL,
+  `width` double NOT NULL,
   PRIMARY KEY (`carport_id`),
-  INDEX `fk_carport_order1_idx` (`order_id` ASC) VISIBLE,
-  CONSTRAINT `fk_carport_order1`
-    FOREIGN KEY (`order_id`)
-    REFERENCES `carport`.`order` (`order_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_carport_order1_idx` (`order_id`),
+  CONSTRAINT `fk_carport_order1` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `carport_has_material_list`
+--
 
--- -----------------------------------------------------
--- Table `carport`.`material`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`material` (
-  `material_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `length` DOUBLE NULL DEFAULT NULL,
-  `width` DOUBLE NULL DEFAULT NULL,
-  `price` DOUBLE NOT NULL,
-  PRIMARY KEY (`material_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `carport_has_material_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carport_has_material_list` (
+  `carport_id` int NOT NULL,
+  `material_id` int NOT NULL,
+  `quantity` float NOT NULL,
+  PRIMARY KEY (`carport_id`,`material_id`),
+  KEY `fk_carport_has_material_list_carport1_idx` (`carport_id`,`material_id`),
+  KEY `fk_carport_has_material_list_material1_idx` (`material_id`),
+  CONSTRAINT `fk_carport_has_material_list_carport1` FOREIGN KEY (`carport_id`) REFERENCES `carport` (`carport_id`),
+  CONSTRAINT `fk_carport_has_material_list_material1` FOREIGN KEY (`material_id`) REFERENCES `material` (`material_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `material`
+--
 
--- -----------------------------------------------------
--- Table `carport`.`carport_has_material_list`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`carport_has_material_list` (
-  `carport_id` INT NOT NULL,
-  `material_id` INT NOT NULL,
-  `quantity` FLOAT NOT NULL,
-  PRIMARY KEY (`carport_id`, `material_id`),
-  INDEX `fk_carport_has_material_list_carport1_idx` (`carport_id` ASC, `material_id` ASC) VISIBLE,
-  INDEX `fk_carport_has_material_list_material1_idx` (`material_id` ASC) VISIBLE,
-  CONSTRAINT `fk_carport_has_material_list_carport1`
-    FOREIGN KEY (`carport_id`)
-    REFERENCES `carport`.`carport` (`carport_id`),
-  CONSTRAINT `fk_carport_has_material_list_material1`
-    FOREIGN KEY (`material_id`)
-    REFERENCES `carport`.`material` (`material_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material` (
+  `material_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `length` double DEFAULT NULL,
+  `width` double DEFAULT NULL,
+  `height` double DEFAULT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`material_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `material_category`
+--
 
--- -----------------------------------------------------
--- Table `carport`.`material_category`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`material_category` (
-  `material_category_id` INT NOT NULL AUTO_INCREMENT,
-  `category_name` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`material_category_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `material_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material_category` (
+  `material_category_id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`material_category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `material_has_material_category`
+--
 
--- -----------------------------------------------------
--- Table `carport`.`material_has_material_category`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`material_has_material_category` (
-  `material_id` INT NOT NULL,
-  `material_category_id` INT NOT NULL,
-  PRIMARY KEY (`material_id`, `material_category_id`),
-  INDEX `fk_material_has_material_category_material_category1_idx` (`material_category_id` ASC) VISIBLE,
-  CONSTRAINT `fk_material_has_material_category_material1`
-    FOREIGN KEY (`material_id`)
-    REFERENCES `carport`.`material` (`material_id`),
-  CONSTRAINT `fk_material_has_material_category_material_category1`
-    FOREIGN KEY (`material_category_id`)
-    REFERENCES `carport`.`material_category` (`material_category_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `material_has_material_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material_has_material_category` (
+  `material_id` int NOT NULL,
+  `material_category_id` int NOT NULL,
+  PRIMARY KEY (`material_id`,`material_category_id`),
+  KEY `fk_material_has_material_category_material_category1_idx` (`material_category_id`),
+  CONSTRAINT `fk_material_has_material_category_material1` FOREIGN KEY (`material_id`) REFERENCES `material` (`material_id`),
+  CONSTRAINT `fk_material_has_material_category_material_category1` FOREIGN KEY (`material_category_id`) REFERENCES `material_category` (`material_category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `order`
+--
 
--- -----------------------------------------------------
--- Table `carport`.`shed`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`shed` (
-  `shed_id` INT NOT NULL AUTO_INCREMENT,
-  `order_id` INT NOT NULL,
-  `total` DOUBLE NULL DEFAULT NULL,
-  `length` DOUBLE NULL DEFAULT NULL,
-  `width` DOUBLE NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `orderdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `order_state` tinyint DEFAULT NULL,
+  PRIMARY KEY (`order_id`,`user_id`),
+  KEY `fk_order_user_idx` (`user_id`),
+  CONSTRAINT `fk_order_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shed`
+--
+
+DROP TABLE IF EXISTS `shed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shed` (
+  `shed_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `total` double DEFAULT NULL,
+  `length` double DEFAULT NULL,
+  `width` double DEFAULT NULL,
   PRIMARY KEY (`shed_id`),
-  INDEX `fk_shed_order1_idx` (`order_id` ASC) VISIBLE,
-  CONSTRAINT `fk_shed_order1`
-    FOREIGN KEY (`order_id`)
-    REFERENCES `carport`.`order` (`order_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  KEY `fk_shed_order1_idx` (`order_id`),
+  CONSTRAINT `fk_shed_order1` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `shed_has_material_list`
+--
 
--- -----------------------------------------------------
--- Table `carport`.`shed_has_material_list`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `carport`.`shed_has_material_list` (
-  `shed_id` INT NOT NULL,
-  `material_id` INT NOT NULL,
-  `quantity` FLOAT NOT NULL,
-  PRIMARY KEY (`material_id`, `shed_id`),
-  INDEX `fk_shed_has_material_list_material1_idx` (`material_id` ASC) VISIBLE,
-  INDEX `fk_shed_has_material_list_shed1_idx` (`shed_id` ASC) VISIBLE,
-  CONSTRAINT `fk_shed_has_material_list_material1`
-    FOREIGN KEY (`material_id`)
-    REFERENCES `carport`.`material` (`material_id`),
-  CONSTRAINT `fk_shed_has_material_list_shed1`
-    FOREIGN KEY (`shed_id`)
-    REFERENCES `carport`.`shed` (`shed_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `shed_has_material_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shed_has_material_list` (
+  `shed_id` int NOT NULL,
+  `material_id` int NOT NULL,
+  `quantity` float NOT NULL,
+  PRIMARY KEY (`material_id`,`shed_id`),
+  KEY `fk_shed_has_material_list_material1_idx` (`material_id`),
+  KEY `fk_shed_has_material_list_shed1_idx` (`shed_id`),
+  CONSTRAINT `fk_shed_has_material_list_material1` FOREIGN KEY (`material_id`) REFERENCES `material` (`material_id`),
+  CONSTRAINT `fk_shed_has_material_list_shed1` FOREIGN KEY (`shed_id`) REFERENCES `shed` (`shed_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `user`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-10 10:44:33
