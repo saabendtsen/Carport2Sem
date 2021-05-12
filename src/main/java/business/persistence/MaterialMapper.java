1821ebb884889b8290dd5a2962fce710cead86e6
August Manniche <august.manniche@gmail.com>
2021-05-12 14:43:23 +0200

    insert into carport_has_material_list

--- src/main/java/business/persistence/MaterialMapper.java
+++ src/main/java/business/persistence/MaterialMapper.java
@@ -61,13 +61,29 @@ public class MaterialMapper {
         //beklædningCalc(order);
 
         order.setStkListe(stkliste);
-
+        carportHasMaterial_list(order);
         //todo: insert stkliste i database på ordre
         return stkliste;
 
 
     }
 
+    public void carportHasMaterial_list(Order order) throws UserException {
+        try (Connection connection = database.connect()) {
+            String sql = "INSERT INTO `carport_has_material_list` (carport_id,material_id,quantity) VALUES (?, ?, ?)";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, order.getCarport().getCarport_id());
+                ps.setDouble(2, order.);
+                ps.setDouble(3, order.);
+                ps.executeUpdate();
+
+            }
+        } catch (SQLException ex) {
+            throw new UserException(ex.getMessage());
+        }
+
+    }
 
     public void beklædningCalc(Order order) {
 
@@ -114,12 +130,12 @@ public class MaterialMapper {
                 if (order.getCarport().getWidth() > 500) {
                     m.setQuantity(3);
                     stkliste.add(m);
-                    stolpeCalc(order,3);
+                    stolpeCalc(order, 3);
                     break;
                 } else {
                     m.setQuantity(2);
                     stkliste.add(m);
-                    stolpeCalc(order,2);
+                    stolpeCalc(order, 2);
                     break;
                 }
             }
@@ -146,9 +162,9 @@ public class MaterialMapper {
                 counter += 1;
             }
         }
-            counter *= remCount;
-            stolpeList.get(0).setQuantity(counter);
-            stkliste.add(stolpeList.get(0));
+        counter *= remCount;
+        stolpeList.get(0).setQuantity(counter);
+        stkliste.add(stolpeList.get(0));
 
     }
 
